4171ac0b3153b507e42d3241b2094976e90e0a54
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-03-13 15:26:27 +0900

    Fix mergeMasterToDevelop.sh

--- mergeMasterToDevelop.sh
+++ mergeMasterToDevelop.sh
@@ -4,7 +4,7 @@ git fetch origin develop:develop
 
 function merge() {
   git checkout $2
-  git pull --rebase origin $1
+  git pull --rebase origin $2
   git merge --no-ff -m "Merge branch '$1' into $2" $1
   git push origin $2
 }
